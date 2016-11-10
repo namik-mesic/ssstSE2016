e074aa28c2833d54796341713e8c2d61898cca0e
Ajla <ajla.leric@ssst.edu.ba>
2016-11-10 12:10:14 +0100

    Calculation

--- app/services/ConverterService.php
+++ app/services/ConverterService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+use Illuminate\Support\Str;
 
 /**
  * Class ConverterService
@@ -14,23 +15,22 @@ class ConverterService
      * @param string $to
      *
      * @return double
-     */
+     *
+     * */
     public function calculate($amount, $from, $to)
     {
-        $url = "http://www.google.com/finance/converter?a=$amount&from=$from&to=$to";
-        $req = curl_init();
-        curl_setopt ($req, CURLOPT_URL, $url);
-        curl_setopt ($req, CURLOPT_RETURNTRANSFER, 1);
-        $time=0;
-        curl_setopt ($req, CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-        curl_setopt ($req, CURLOPT_CONNECTTIMEOUT, $time);
-        $r= curl_exec($req);
-        curl_close($req);
-
-        $pos = strpos($r, 'class=bld');
+        $amount = urlencode($amount);
+        $from = urlencode($from);
+        $to= urlencode($to);
+        $get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to");
+        $get = explode("<span class=bld>",$get);
+        $get = explode("</span>",$get[1]);
+        $r = preg_replace("/[^0-9\.]/", null, $get[0]);
 
         return $r;
     }
+
+
 
 
 }
