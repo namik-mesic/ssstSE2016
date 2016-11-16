d1764a1990d70d15732bf65fa49013dbd8c1cf49
Edim Bogdanic <edim.bogdanic@stu.ssst.edu.ba>
2016-11-16 16:59:32 +0100

    Added methods for rollback on all the current migrations

--- database/migrations/2016_11_12_143529_add_username_value_on_table_users.php
+++ database/migrations/2016_11_12_143529_add_username_value_on_table_users.php
@@ -26,7 +26,7 @@ class AddUsernameValueOnTableUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('username');
         });
     }
 }
