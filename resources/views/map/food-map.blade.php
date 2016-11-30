6301ec0c06b0777bd531cf6d224f61cc9a610512
ademhadrovic <adem.hadrovic@stu.ssst.edu.ba>
2016-11-30 20:50:42 +0100

    implementing working solution for getting the user's geolocation properly in food map

--- resources/views/map/food-map.blade.php
+++ resources/views/map/food-map.blade.php
@@ -10,37 +10,67 @@
         //<![CDATA[
 
         var map;
-        var infowindow;
 
 
         <!-- Creates a map where Sebilj and shows the nearest places to get food -->
         function initMap() {
-            var pyrmont = {lat: 43.860702, lng: 18.429932};
 
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: pyrmont,
-                mapTypeControl: false,
-                streetViewControl: false,
-                zoom: 16
-            });
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
 
 
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: pyrmont,
-                radius: 500,
-                type: ['restaurant'],
-                type: ['food'],
-                type: ['meal_takeaway'],
-                type: ['meal_delivery'],
-                type: ['bakery']
-            }, callback);
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: pos,
+                        mapTypeControl: false,
+                        streetViewControl: false,
+                        zoom: 16
+                    });
 
-            // this line returns undefined vvvv
-            //service = new google.maps.places.PlacesService(map);
-            //service.nearbySearch(request, callback);
+                    // {map:map} will display the "you are here" bubble
+                    infoWindow = new google.maps.InfoWindow({map: map});
+                    infoWindow.setContent("You are here");
+
+                    var service = new google.maps.places.PlacesService(map);
+                    service.nearbySearch({
+                        location: pos,
+                        radius: 500,
+                        type: ['restaurant'],
+                        type: ['food'],
+                        type: ['meal_takeaway'],
+                        type: ['meal_delivery'],
+                        type: ['bakery']
+                    }, callback);
+
+                    // this line returns undefined vvvv
+                    //service = new google.maps.places.PlacesService(map);
+                    //service.nearbySearch(request, callback);
+
+                }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            }
+
+            else {
+                // If browser doesn't support Geolocation,
+                // set location to default (sebilj)
+                var coordinates = {lat: 43.860702, lng: 18.429932};
+                handleLocationError(false, infoWindow, coordinates);
+
+            }
+
         }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                    'Error: The Geolocation service failed.' :
+                    'Error: Your browser doesn\'t support geolocation.');
+        }
+
 
         <!-- Calls the Google API for each marker needed -->
         function callback(results, status) {
