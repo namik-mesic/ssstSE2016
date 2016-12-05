da6176fd7bbd2a9263cef9edb1bae5c24fda1e64
SanEdis <edis.sijaric@stu.ssst.edu.ba>
2016-12-05 13:18:43 +0100

    Contact us page

--- resources/views/Currency/contact.blade.php
+++ resources/views/Currency/contact.blade.php
@@ -1,6 +1,11 @@
 @extends('main')
 @section('content')
-
+<h2> Edis Sijaric </h2>
+    edis.sijaric@stu.ssst.edu.ba
+    <h2> Ajla Leric </h2>
+    ajla.leric@stu.ssst.edu.ba
+    <h2>Amina Halidovic</h2>
+    amina.halidovic@stu.ssst.edu.ba
 
 
 @endsection
