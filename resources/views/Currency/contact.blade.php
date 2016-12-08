b7d926e023651381f78551dc54f76020c9eb95d1
SanEdis <edis.sijaric@stu.ssst.edu.ba>
2016-12-08 16:48:58 +0100

    Commit

--- resources/views/Currency/contact.blade.php
+++ resources/views/Currency/contact.blade.php
@@ -2,10 +2,13 @@
 @section('content')
 <h2> Edis Sijaric </h2>
     edis.sijaric@stu.ssst.edu.ba
+    061/589-826
     <h2> Ajla Leric </h2>
     ajla.leric@stu.ssst.edu.ba
+    061/111-222
     <h2>Amina Halidovic</h2>
     amina.halidovic@stu.ssst.edu.ba
+    060/325-99-58
 
 
 @endsection
