5ed99a6533ab0ce1fb8d42082e2af0e0098eb58d
ademhadrovic <adem.hadrovic@stu.ssst.edu.ba>
2016-12-23 23:07:50 +0100

    Created PlaceDBController File

---
+++ app/Http/Controllers/Api/PlaceDataController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Place;
+use Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+/**
+ * Class PlaceController
+ * @package App\Http\Controllers
+ */
+class Place extends Controller
+{
+   /**
+    * returns places array
+    * from DB
+
+    */
+    public function index(){
+
+        //return places
+
+    }
+
+
+
+}
