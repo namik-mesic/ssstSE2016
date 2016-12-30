74038943a34ac7dd5532f7df5cedc222dddba8af
nedzadhafizovic <nedzad.hafizovic@stu.ssst.edu.ba>
2016-12-30 22:06:22 +0100

    Added default avatar

--- database/migrations/2016_12_28_161851_create_imgPath_filed_in_tabel_users.php
+++ database/migrations/2016_12_28_161851_create_imgPath_filed_in_tabel_users.php
@@ -14,7 +14,7 @@ class CreateImgPathFiledInTabelUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('imgPath');
+            $table->string('imgPath')->default('http://smtp.icimod.org/girc/dmis/img/user-avatar-placeholder.png');
         });
     }
 
