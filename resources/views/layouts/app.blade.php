b483477b8bb05bf59ea2eeaa0764fa30b96dfe4b
Dino <sabkod@hotmail.com>
2017-01-01 18:07:30 +0100

    Layout structure changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,10 +39,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <div class="wrapper">
     <!-- Header -->
-    @include('header')
+    @include('layouts.header')
 
     <!-- Sidebar -->
-    @include('sidebar')
+    @include('layouts.sidebar')
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -67,7 +67,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div><!-- /.content-wrapper -->
 
     <!-- Footer -->
-    @include('footer')
+    @include('layouts.footer')
 
 </div><!-- ./wrapper -->
 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+            <!-- To the right -->
+            <div class="pull-right hidden-xs">
+                BytePack
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright © 2016 <a href="/">MailBird</a>.</strong> All rights reserved.
+        </footer>
