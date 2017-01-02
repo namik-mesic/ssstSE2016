ea3d933318dcdd289b00affabda0a435f893978b
Dino <sabkod@hotmail.com>
2017-01-02 16:28:51 +0100

    Cleanup

--- resources/views/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-            <!-- To the right -->
-            <div class="pull-right hidden-xs">
-                BytePack
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright © 2016 <a href="/">MailBird</a>.</strong> All rights reserved.
-        </footer>
