5aa6af512e50c6ea2f00681daacfaa2797d39ab7
Dino <sabkod@hotmail.com>
2017-01-06 16:21:20 +0100

    Added schedules list in menu sidebar

---
+++ resources/views/campaignschedule/archived.blade.php
@@ -0,0 +1,44 @@
+@extends( 'layouts.app' )
+@section( 'content' )
+
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Archived Scheduled Campaigns </h3>
+
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Mail Name</th>
+                    <th>Subject</th>
+                    <th>Mailing list</th>
+                    <th>Status</th>
+
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($campaignschedules as $campaignschedule)
+                    <tr> @if ($campaignschedule -> status == 'sent')
+                        <td>{{$campaignschedule->campaign->name}}</td>
+                        <td>{{$campaignschedule->campaign->subject}}</td>
+                        <td>{{$campaignschedule->mailingList->name}}</td>
+                        <td>{{$campaignschedule->status}}</td>
+                        @endif
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+
+            </table>
+            </br>
+
+        </div>
+    </div>
+    </div>
+    <!-- /.box -->
+
+@stop
