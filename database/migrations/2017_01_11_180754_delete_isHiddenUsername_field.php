7a544797aea4d75019698bf27f4e0cb9b3cdc117
Dalila Isanovic <dalila.isanovic@stu.ssst.edu.ba>
2017-01-11 19:26:58 +0100

    deleting the isUsernameHidden field from db

---
+++ database/migrations/2017_01_11_180754_delete_isHiddenUsername_field.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteIsHiddenUsernameField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*
+         * The username should not be set to private
+         * so we don't need this field - Dalila*/
+        
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('isUsernameHidden');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('isUsernameHidden')->nullable();
+        });
+    }
+}
