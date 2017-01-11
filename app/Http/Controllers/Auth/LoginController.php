15c30cbfe39a4de78e00ee4c1732113e5db93002
= <damir.cengic@stu.ssst.edu.ba>
2017-01-11 15:07:21 +0100

    changed redirects after login to new dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard'; //damir cengic
 
     /**
      * Create a new controller instance.
