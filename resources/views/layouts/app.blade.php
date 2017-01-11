54a38544d46a554a696d411b8c9b5e33fb331439
= <damir.cengic@stu.ssst.edu.ba>
2017-01-11 20:12:29 +0100

    minor fixes for password meter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,8 +69,5 @@
 <!-- AdminLTE App -->
 <script src="{{ asset ("/bower_components/AdminLTE/dist/js/app.min.js") }}" type="text/javascript"></script>
 
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-      Both of these plugins are recommended to enhance the
-      user experience -->
 </body>
 </html>
